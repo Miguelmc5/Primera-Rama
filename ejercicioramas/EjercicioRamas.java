7ada9eddec1aff048fdec793ab796abcfced740d
Miguelmc5 <mmartinezcosta@danielcastelao.org>
2020-01-10 13:49:13 +0100

    un commit cualquiera

---
+++ ejercicioramas/EjercicioRamas.java
@@ -0,0 +1,18 @@
+package ejercicioramas;
+
+/**
+ *
+ * @author DANIELCASTELAO\mmartinezcosta
+ */
+public class EjercicioRamas {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+        System.out.println("Hola chavules");
+    
+    
+    }
+    
+}
